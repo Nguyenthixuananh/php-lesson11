fe28275afe99bdeb359e88369a11e0fba05a5032
nguyenthixuananh <nguyenhixuananh09061999@gmail.com>
2021-10-21 15:09:07 +0700

    Hotel money

--- Tinh tien phong hotel/index.php
+++ Tinh tien phong hotel/index.php
@@ -4,13 +4,22 @@ $arr = [
     [0, 5, 0, 0],
     [2, 0, 3, 3]
 ];
-$sum=0;
-for ($i=0;$i<count($arr[0]);$i++){
-    for ($j=0;$j<count($arr);$j++){
-        if($arr[$j][$i]==0){
-            break;
+function sum($array)
+{
+    $sum = 0;
+    for ($i = 0; $i < count($array); $i++) {
+        for ($j = 0; $j < count($array[$i]); $j++) {
+            if ($array[$i][$j] == 0)
+                continue;
+            if($i > 0)
+                if($array[$i-1][$j] == 0)
+                    continue;
+            $sum += $array[$i][$j];
+
+
         }
-        $sum+=$arr[$j][$i];
     }
+    return $sum;
 }
-echo $sum;
+
+echo sum($arr);
